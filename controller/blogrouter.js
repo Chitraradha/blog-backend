1e0e5bdd0bc74b86b92fc1aa16a9494d71f27933
Chitraradha <chitrak1110@gmail.com>
2024-02-10 22:23:59 +0530

    create

--- controller/blogrouter.js
+++ controller/blogrouter.js
@@ -66,6 +66,9 @@ router.post("/add",async(req,res)=>{
     })
  })
 
-
+ router.get("/viewall",async(req,res)=>{
+    let data=await userModel.find()
+    res.json(data)
+ })
 
 module.exports=router 
